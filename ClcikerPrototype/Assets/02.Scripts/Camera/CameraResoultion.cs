7c1b25c5a8004b77cf590b66dc837d6384706d29
COCONUT <rnjstjdqls9731@gmail.com>
2022-11-11 01:38:41 +0900

    UI 재현 중

---
+++ ClcikerPrototype/Assets/02.Scripts/Camera/CameraResoultion.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraResoultion : MonoBehaviour
+{
+    void Awake()
+    {
+        SetGameResolution();
+    }
+    void SetGameResolution()
+    {
+        Camera.main.orthographicSize *= ((float)Screen.height / Screen.width) / (1920f / 1080);
+        Camera.main.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, 1920f / 1080 * Screen.width / 2, 0));
+    }
+}
